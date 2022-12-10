f8d45889f1e7aac4a5ebb3db67178c41ba95edcd
willthom1080 <102179711+willthom1080@users.noreply.github.com>
2022-12-09 21:09:16 -0500

    Stuff

--- Assets/Scripts/Overworld/PlayerMovementS.cs
+++ Assets/Scripts/Overworld/PlayerMovementS.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovementS : MonoBehaviour
 {
     Rigidbody2D body;
+
+    Camera mainCam;
 
     float horizontal;
     float vertical;
@@ -13,7 +16,9 @@ public class PlayerMovementS : MonoBehaviour
     public float runSpeed;
     void Start()
     {
+        mainCam = Camera.main;
         body = GetComponent<Rigidbody2D>();
+        mainCam.transform.position = new Vector3(body.position.x, body.position.y, -10);
     }
 
     // Update is called once per frame
@@ -21,6 +26,10 @@ public class PlayerMovementS : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        if (Mathf.Abs(horizontal) > 0.001 || Mathf.Abs(vertical) > 0.001)
+        {
+            mainCam.transform.position = new Vector3(body.position.x, body.position.y, -10);
+        }
     }
 
     private void FixedUpdate()
@@ -31,5 +40,6 @@ public class PlayerMovementS : MonoBehaviour
             vertical *= diagRatio;
         }
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        
     }
 }
