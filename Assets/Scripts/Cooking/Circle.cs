c120194b25de8e3c1e9cbb873d506ebeda2a7f1f
willthom1080 <102179711+willthom1080@users.noreply.github.com>
2022-12-29 21:33:16 -0500

    adding extent tracking for cooking

--- Assets/Scripts/Cooking/Circle.cs
+++ Assets/Scripts/Cooking/Circle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Circle : MonoBehaviour
 {
     public bool heldDown;
+    public float extent;
     private void OnMouseDown()
     {
         heldDown = true;
@@ -23,7 +24,10 @@ public class Circle : MonoBehaviour
         {
             Vector3 tempVect;
             tempVect = Camera.main.ScreenToWorldPoint(new Vector3(0, Input.mousePosition.y, 10));
-            transform.position = new Vector3(0, tempVect.y, tempVect.z);
+            if ((!(Mathf.Abs(tempVect.y) > extent)))
+            {
+                transform.position = new Vector3(0, tempVect.y, tempVect.z);
+            }
             if (Input.GetMouseButtonDown(0))
             {
                 heldDown = true;
@@ -32,7 +36,7 @@ public class Circle : MonoBehaviour
             else if (Input.GetMouseButtonUp(0))
             {
                 heldDown = false;
-                transform.position = new Vector3(0, 4, 0);
+                transform.position = new Vector3(0, extent, 0);
             }
         }
     }
